03032daa58264d8b50766425d577ae46dfd81d30
nagiatzi <nagiatzi.com>
2018-01-06 07:34:14 +0200

    adding files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Example from './components/navbar'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Example />
+      </div>
+    );
+  }
+}
+
+export default App;
