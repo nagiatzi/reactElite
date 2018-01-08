b6a343cd7c9a8903dc8c656643f4ab0246a24233
nagiatzi <nagiatzi.com>
2018-01-08 18:42:43 +0200

    improvements

--- src/components/product1.js
+++ src/components/product1.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
+import './product.css'
 
 
 class Product1 extends Component {
@@ -25,10 +25,11 @@ toggle() {
             <Container>
               <Row>
                 <Col lg="5" className="text-center ml-auto">
+                  <div>
                   <hr className="section-heading-spacer"/>
                   <h2 className="section-heading">  Biomass briquettes </h2>
                   <p class="lead">  Οι
-                  <a target="_blank" href="https://el.wikipedia.org/wiki/%CE%9C%CF%80%CF%81%CE%B9%CE%BA%CE%AD%CF%84%CE%B1"> μπρικέτες</a> τύπου Βιομας, αποτελούν ουσιαστικά την τελευταία λέξη της τεχνολογίας στον χώρο της παραγωγής μπρικετών. Ιδανική λύση για κάθε επαγγελματία αλλά και ιδιώτη διαθέσιμη στον χώρο σας με ένα τηλεφώνημα. <Button  onClick={this.toggle} className="pageBtn">Περισσότερα </Button></p>
+                  <a  className="productLink" target="_blank" href="https://el.wikipedia.org/wiki/%CE%9C%CF%80%CF%81%CE%B9%CE%BA%CE%AD%CF%84%CE%B1"> μπρικέτες</a> τύπου Βιομας, αποτελούν ουσιαστικά την τελευταία λέξη της τεχνολογίας στον χώρο της παραγωγής μπρικετών. Ιδανική λύση για κάθε επαγγελματία αλλά και ιδιώτη διαθέσιμη στον χώρο σας με ένα τηλεφώνημα. <Button  onClick={this.toggle} id="pageBtn">Περισσότερα </Button></p>
                   <Modal isOpen={this.state.modal} toggle={this.toggle} className="modal1">
                     <ModalHeader toggle={this.toggle}>Biomass briquettes</ModalHeader>
                     <ModalBody>
@@ -38,6 +39,7 @@ toggle() {
                       <Button outline color="secondary" onClick={this.toggle}>Κλείσιμο</Button>
                     </ModalFooter>
                   </Modal>
+                  </div>
                 </Col>
                 <Col lg="5" className="ml-auto">
                   <img class="img-fluid" src={ require('../img/product1.png') } alt="μπρικέτα βιομάς Κομοτηνή"/>
