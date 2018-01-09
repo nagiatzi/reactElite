08c8cb05550f50049aebe7cb729a4ecc281d712e
nagiatzi <nagiatzi.com>
2018-01-09 18:38:06 +0200

    icons added

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,6 +4,11 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faPhone from '@fortawesome/fontawesome-free-solid/faPhone'
 import faHome from '@fortawesome/fontawesome-free-solid/faHome'
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook'
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram'
+
+
+
 import './product.css';
 
 class Contact extends Component {
@@ -17,7 +22,21 @@ class Contact extends Component {
                 <Col xs={false} lg="6" className="my-auto">
                   <h2>Επικοινωνήστε μαζί μας στα τηλέφωνα: <h3><hr /><FontAwesomeIcon icon={faPhone} className="phoneIcon"/> 6970019053 <hr /> <FontAwesomeIcon icon={faHome} className="phoneIcon"/> 2031033954 </h3></h2>
                 </Col>
-                <Col xs={false} lg="5" className="ml-auto">
+                <Col xs={false} lg="6" className="my-auto">
+                            <ul className="list-inline banner-social-buttons">
+                              <li className="list-inline-item">
+                                <a href="#" class="btn btn-secondary btn-lg">
+                                  <FontAwesomeIcon icon={faFacebook} className="fa-fw"/>
+                                <span className="network-name"> Facebook</span>
+                                </a>
+                              </li>
+                              <li className="list-inline-item">
+                                <a href="#" className="btn btn-secondary btn-lg" id="koubi2">
+                                  <FontAwesomeIcon icon={faInstagram} className="fa-fw"/>
+                                <span className="network-name">Instagram</span>
+                              </a>
+                            </li>
+                          </ul>
                 </Col>
               </Row>
             </Container>
